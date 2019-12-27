87d3aeb548e124ae67bf846cc426315fe9f5ce1f
CanT <taalaay@gmail.com>
2019-12-27 12:41:54 +0300

    Initial Commit

---
+++ MyApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyApplication
+//
+//  Created by Can Talay on 27.12.2019.
+//  Copyright © 2019 Can Talay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
